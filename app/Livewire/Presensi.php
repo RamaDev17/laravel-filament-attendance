87f4d8d2f0d948b478f24a765e8d39142b0bc16a
RamaDev17 <ramdhaney.dev17@gmail.com>
2024-10-03 18:49:38 +0700

    leaflet js

--- app/Livewire/Presensi.php
+++ app/Livewire/Presensi.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Schedul;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Presensi extends Component
 {
     public function render()
     {
-        return view('livewire.presensi');
+        $schedule = Schedul::where('user_id', Auth::user()->id)->first();
+        return view('livewire.presensi', [
+            'schedule' => $schedule
+        ]);
     }
 }
--- resources/views/livewire/presensi.blade.php
+++ resources/views/livewire/presensi.blade.php
@@ -7,8 +7,8 @@
                     <h2 class="text-2xl font-bold mb-2"> Informasi Pegawai</h2>
                     <div class="bg-gray-100 p-4 rounded-lg">
                         <p class="mb-2"><strong>Nama Pegawai:</strong> {{ Auth::user()->name }}</p></p>
-                        <p class="mb-2"><strong>Kantor:</strong> Kantor Utama</p></p>
-                        <p class="mb-2"><strong>Shift:</strong> Pagi</p></p>
+                        <p class="mb-2"><strong>Kantor:</strong> {{$schedule->office->name}}</p></p>
+                        <p class="mb-2"><strong>Shift:</strong> {{$schedule->shift->name}} ({{$schedule->shift->start_time}} - {{$schedule->shift->end_time}}) WIB</p></p>
                     </div>
                 </div>
                 <div>
@@ -16,15 +16,43 @@
                     <div id="map" class="mb-4 rounded-lg border border-gray-300">
 
                     </div>
-                    <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tag location</button>
+                    <button type="button" onclick="tagLocation()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tag location</button>
+                    <button type="button" onclick="" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Presence</button>
                 </div>
             </div>
         </div>
     </div>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     <script>
-        const map = L.map('map').setView([-6.175110, 106.865036], 13);
+        const map = L.map('map').setView([{{$schedule->office->latitude}}, {{$schedule->office->longitude}}], 18);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+        const center = [{{$schedule->office->latitude}}, {{$schedule->office->longitude}}];
+        const radius = {{$schedule->office->radius}};
+
+        let marker;
+
+        const circle = L.circle(center, {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius
+        }).addTo(map);
+
+        function tagLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const lat = position.coords.latitude;
+                    const lng = position.coords.longitude;
+
+                    if (marker) {
+                        map.removeLayer(marker);
+                    }
+                    marker = L.marker([lat, lng]).addTo(map);
+                    map.setView([lat, lng], 18);
+                })
+            }
+        }
 
     </script>
 </div>
